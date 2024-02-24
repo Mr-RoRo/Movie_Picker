8b918fe6a13237688c8b91ae17ef86605a54c586
Danial <fsoocity@gmail.com>
2024-02-24 16:18:53 +0330

    feat(): add top rated Movie

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,16 +3,22 @@ import Slider from "../../components/Slider";
 import useMoviesGenres from "../../hooks/useMoviesGenres";
 import usePopularMovies from "../../hooks/usePopularMovies";
 import useTrendingMovies from "../../hooks/useTrendingMovies";
+import useUpcomingMovies from "../../hooks/useTopRatedMovies";
 
 const HomePage = () => {
   const { data: trendingMovies } = useTrendingMovies();
   const { data: moviesGenres } = useMoviesGenres();
   const { data: popularMovie } = usePopularMovies();
+  const { data: upcomingMovies } = useUpcomingMovies();
   return (
     <div>
       <Slider Movies={trendingMovies?.results} MoviesGenres={moviesGenres} />
       <FreeModeSlider
         Movies={popularMovie?.results}
+        MoviesGenres={moviesGenres}
+      />
+      <FreeModeSlider
+        Movies={upcomingMovies?.results}
         MoviesGenres={moviesGenres}
       />
     </div>
