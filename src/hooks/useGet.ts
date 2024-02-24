3f7a9a400718edc7783a502f601596c34d58d24c
Danial <fsoocity@gmail.com>
2024-02-24 22:39:16 +0330

    feat(): add staleTime

--- src/hooks/useGet.ts
+++ src/hooks/useGet.ts
@@ -1,12 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import ApiClient from "../services/ApiClient";
 
-
-const useGet = <T>(endpoint: string, queryKey: string[]) => {
+const useGet = <T>(
+  endpoint: string,
+  queryKey: string[],
+  staleTime?: number
+) => {
   const apiClient = new ApiClient<T>(endpoint);
   return useQuery<T, Error>({
     queryKey: queryKey,
     queryFn: apiClient.get,
+    staleTime: staleTime,
   });
 };
 
