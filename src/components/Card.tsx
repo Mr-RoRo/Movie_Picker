afbbe2ba8741279bfd014fa70d4667d2dee7bf84
Danial <fsoocity@gmail.com>
2024-02-25 18:18:59 +0330

    feat(): create movie and tv show card

---
+++ src/components/Card.tsx
@@ -0,0 +1,44 @@
+import { AsyncImage } from "loadable-image";
+import { Genres, MovieAndTVshow } from "../hooks/types";
+
+const Card = ({
+  movie,
+  MoviesGenres,
+  className,
+}: {
+  movie: MovieAndTVshow;
+  MoviesGenres?: Genres;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={` relative text-white  rounded-3xl overflow-hidden text-lg ${className}`}
+    >
+      <AsyncImage
+        src={"https://image.tmdb.org/t/p/w300" + movie.poster_path}
+        loader={<div className="skeleton " />}
+        className="w-full h-full"
+        style={{ objectFit: "fill" }}
+      />
+      <span className="bg-black opacity-25 w-full h-full absolute top-0" />
+      <h1 className="absolute top-3 left-2 font-extrabold text-lg sm:text-xl lg:text-1xl xl:text-2xl">
+        {movie.title || movie.name}
+      </h1>
+
+      <span className="badge badge-lg glass absolute bottom-2 left-2 font-semibold">
+        {
+          MoviesGenres?.genres.find((gen) => movie.genre_ids.includes(gen.id))
+            ?.name
+        }
+      </span>
+      <span className="badge badge-lg glass absolute bottom-9 left-2 font-semibold">
+        {Number(movie.vote_average).toFixed(1)}
+      </span>
+      <button className="btn glass absolute bottom-2.5 right-2">
+        Show Details
+      </button>
+    </div>
+  );
+};
+
+export default Card;
