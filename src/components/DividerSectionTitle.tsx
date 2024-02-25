64e024b246fde37b3594767bcec5052dcbecc3dd
Danial <fsoocity@gmail.com>
2024-02-25 16:38:23 +0330

    fix(): make Divider Title reusable

--- src/components/DividerSectionTitle.tsx
+++ src/components/DividerSectionTitle.tsx
@@ -1,6 +1,14 @@
-const DividerSectionTitle = ({ Title }: { Title: string }) => {
+const DividerSectionTitle = ({
+  Title,
+  className,
+}: {
+  Title: string;
+  className?: string;
+}) => {
   return (
-    <div className="divider divider-end text-xl sm:text-2xl lg:text-3xl font-bold my-10">
+    <div
+      className={`divider divider-end text-xl sm:text-2xl lg:text-3xl font-bold ${className}`}
+    >
       {Title}
     </div>
   );
