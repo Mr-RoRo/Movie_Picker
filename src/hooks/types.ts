331bcc191664151ff914ccf1f2db1df4bfc3edfc
Danial <fsoocity@gmail.com>
2024-02-25 03:35:13 +0330

    fix(): add first_air_date for tv shows

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -13,6 +13,7 @@ export interface MovieAndTVshow {
   genre_ids: number[];
   popularity: number;
   release_date: string;
+  first_air_date: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
