6f77050c6566f94140ce4b3094c75100bbb7ef84
Danial <fsoocity@gmail.com>
2024-02-26 16:39:14 +0330

    feat(): add axios config to hook

--- src/hooks/useGet.ts
+++ src/hooks/useGet.ts
@@ -1,15 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import ApiClient from "../services/ApiClient";
+import { AxiosRequestConfig } from "axios";
 
 const useGet = <T>(
   endpoint: string,
   queryKey: string[],
-  staleTime?: number
+  staleTime?: number,
+  config?: AxiosRequestConfig
 ) => {
   const apiClient = new ApiClient<T>(endpoint);
   return useQuery<T, Error>({
     queryKey: queryKey,
-    queryFn: apiClient.get,
+    queryFn: () => {
+      return apiClient.get(config);
+    },
     staleTime: staleTime,
   });
 };
