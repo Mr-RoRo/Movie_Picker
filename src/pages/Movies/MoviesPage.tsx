1314497fe90ef498ead546904ecbb2733e9a2ddc
Danial <fsoocity@gmail.com>
2024-02-27 13:31:24 +0330

    feat(): add infinite scroll

--- src/pages/Movies/MoviesPage.tsx
+++ src/pages/Movies/MoviesPage.tsx
@@ -1,25 +1,28 @@
+import InfiniteScroll from "react-infinite-scroll-component";
 import Card from "../../components/Card";
 import SearchBar from "../../components/SearchBar";
 import { MovieAndTVshow } from "../../hooks/types";
-import useGet from "../../hooks/useGet";
-import { FetchResponse } from "../../services/ApiClient";
+import useInfiniteGet from "../../hooks/useInfiniteGet";
 import { useGenreStore, useSearch } from "../../store";
 
 const MoviesPage = () => {
   const search = useSearch((s) => s.search);
 
-  const { data: movies } = useGet<FetchResponse<MovieAndTVshow>>(
-    "discover/movie",
-    ["movies"]
-  );
+  const {
+    data: movies,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteGet<MovieAndTVshow>("discover/movie", ["movies"]);
 
-  const { data: SearchedMovies, isLoading } = useGet<
-    FetchResponse<MovieAndTVshow>
-  >(`search/movie`, ["SearchedMovies", search], 60 * 1000, {
-    params: {
-      query: search,
-    },
-  });
+  const {
+    data: searchedMovies,
+    isLoading,
+    hasNextPage: searchedMoviesHasNextPage,
+    fetchNextPage: searchedMoviesNextPage,
+  } = useInfiniteGet<MovieAndTVshow>(`search/movie?query=${search}`, [
+    "SearchedMovies",
+    search,
+  ]);
 
   const genres = useGenreStore((s) => s.genres);
   return (
@@ -28,13 +31,39 @@ const MoviesPage = () => {
       {isLoading ? (
         <span className="loading loading-infinity loading-lg self-center"></span>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-7">
-          <Card
-            movies={search ? SearchedMovies?.results : movies?.results}
-            MoviesGenres={genres}
-            className="w-60 h-[18.5rem] mx-auto"
-          />
-        </div>
+        <InfiniteScroll
+          dataLength={
+            searchedMovies?.pages[0].total_results
+              ? searchedMovies.pages.length || 1
+              : movies?.pages.length || 1
+          }
+          hasMore={
+            searchedMovies?.pages[0].total_results
+              ? searchedMoviesHasNextPage
+              : hasNextPage
+          }
+          next={
+            searchedMovies?.pages[0].total_results
+              ? searchedMoviesNextPage
+              : fetchNextPage
+          }
+          loader={
+            <span className="loading loading-infinity loading-lg self-center"></span>
+          }
+        >
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-x-4 gap-y-7">
+            {(searchedMovies?.pages[0].total_results
+              ? searchedMovies
+              : movies
+            )?.pages.map((movie) => (
+              <Card
+                movies={movie.results}
+                MoviesGenres={genres}
+                className="w-60 h-[18.5rem] mx-auto"
+              />
+            ))}
+          </div>
+        </InfiniteScroll>
       )}
     </div>
   );
