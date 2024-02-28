01829629c64af4a78f4aeab66833a52903c73168
Danial <fsoocity@gmail.com>
2024-02-28 16:26:30 +0330

    add(types): add new types

--- src/hooks/types.ts
+++ src/hooks/types.ts
@@ -28,3 +28,66 @@ export interface SortedItems {
   name: string;
   label: string;
 }
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: string;
+  budget: number;
+  genres: Genres[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+export interface Reviews {
+  id: string;
+  author: string;
+  author_details: AuthorDetails;
+  content: string;
+  created_at: string;
+  updated_at: string;
+  url: string;
+}
+
+interface AuthorDetails {
+  name: string;
+  username: string;
+  avatar_path: string;
+  rating: string;
+}
