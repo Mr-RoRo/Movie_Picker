df4fc0ef4264395fd6f4189f3c73eafccda6d59f
Danial <fsoocity@gmail.com>
2024-02-29 14:21:20 +0330

    feat(): add tv show details page

--- src/pages/layout/router.tsx
+++ src/pages/layout/router.tsx
@@ -4,6 +4,7 @@ import Layout from "./Layout";
 import MoviesPage from "../Movies/MoviesPage";
 import TVshows from "../TVshows/TVshows";
 import MovieDetails from "../Movies/MovieDetails";
+import TVshowDetails from "../TVshows/TVshowDetails";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
       { path: "movies", element: <MoviesPage /> },
       { path: "movies/:id", element: <MovieDetails /> },
       { path: "tvshows", element: <TVshows /> },
+      { path: "tvshows/:id", element: <TVshowDetails /> },
     ],
   },
 ]);
